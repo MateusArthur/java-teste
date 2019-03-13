88bdfbcf4882fc468dd06e6c3fa1d2428a014207
Leonardo Soares <soaresleonardo06@gmail.com>
2019-03-13 11:27:04 -0300

    TESTANDO JAVA

---
+++ Main.java
@@ -0,0 +1,10 @@
+class Main {
+	public static void main(String args[]){
+		Pessoa a;
+		a = new Pessoa();
+		a.idade = 19;
+		a.anoNascimento = 2000;
+		a.altura = 1.74;
+		System.out.println(a.idade);
+	}
+}
